23a8b15a56e1cf68047eef55cd7e4dc7560fd4c0
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-04 23:33:55 +0100

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,27 @@
+class Main(){
+     adds(a,b){
+         return a - b;
+     
+
+     }
+
+
+     subs(a,b){
+          return a + b;
+     
+
+     }
+
+    bcc(a){
+        b=0;
+        if a==0 {
+            b=1;
+        }
+    }
+
+    main(){
+        printf "hello world...\n";
+        return 0;
+    }
+
+}
